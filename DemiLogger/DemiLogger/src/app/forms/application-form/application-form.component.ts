787d5379af6ebdda91afbf30b1859dd5963e9c66
Danie <27678822@student.g.nwu.ac.za>
2018-09-08 16:46:32 +0200

    Website-updates/login-form-changed/application-form-changed

--- DemiLogger/DemiLogger/src/app/forms/application-form/application-form.component.ts
+++ DemiLogger/DemiLogger/src/app/forms/application-form/application-form.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./application-form.component.css']
 })
 export class ApplicationFormComponent implements OnInit {
+  gender = ['male', 'female'];
+  maritalStatus = ['Single', 'Married', 'Divorced', 'Widowed'];
+  correspondancePreference = ['English', 'Afrikaans'];
+  race = ['African', 'Coloured', 'Indian', 'White', 'Other'];
+  previouslyemployed = ['Yes', 'No'];
+  otheremployer = ['Yes', 'No'];
+  selfemployed = ['Yes', 'No'];
+  accounttype = ['Current(cheque)', 'Savings', 'Transmission'];
+  accountinfo = ['Own account', 'Joint account', '3rd party'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: ngForm)
+  {
+    console.log(form);
   }
 
 }
--- DemiLogger/DemiLogger/src/app/login/login.component.ts
+++ DemiLogger/DemiLogger/src/app/login/login.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  // defaultQuestion = 'pet';
+  // answer = '';
+  // gender = ['male', 'female'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: ngForm)
+  {
+    console.log(form);
   }
 
 }
