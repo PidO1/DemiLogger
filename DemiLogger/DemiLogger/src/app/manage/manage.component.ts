5f59eb1bab818aeed1dccd44de958f503085dda3
Danie <27678822@student.g.nwu.ac.za>
2018-09-27 00:09:04 +0200

    Website-updates/Form-updates

--- DemiLogger/DemiLogger/src/app/manage/manage.component.ts
+++ DemiLogger/DemiLogger/src/app/manage/manage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { PgpdServiceService } from "../pgpd-service.service";
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-manage',
@@ -8,7 +9,7 @@ import { PgpdServiceService } from "../pgpd-service.service";
   styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
-
+  nwunumberjson: string = '';
 
   constructor(private submitService: PgpdServiceService) { }
 
@@ -30,6 +31,26 @@ export class ManageComponent implements OnInit {
     this.submitService.storeLecturerData(form.value)
       .subscribe(
         (response) => console.log(response),
+        (error) => console.log(error)
+      );
+  }
+
+  onGetStudentApplication()
+  {
+    this.submitService.getStudentInfo()
+      .subscribe(
+        (response: Response) => {
+          const data = response.json();
+          // for (const studentInfo of data)
+          // {
+          //   studentInfo.nwunumber = 'nwu' + studentInfo.nwunumber;
+          //   studentInfo.password;
+          //   console.log(studentInfo)
+          // }
+          console.log(data)
+          this.nwunumberjson = data.nwunumber;
+          console.log(this.nwunumberjson)
+        },
         (error) => console.log(error)
       );
   }
--- DemiLogger/DemiLogger/src/app/pgpd-service.service.ts
+++ DemiLogger/DemiLogger/src/app/pgpd-service.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Http, Headers} from "@angular/http";
 
 @Injectable({
@@ -6,7 +6,8 @@ import {Http, Headers} from "@angular/http";
 })
 export class PgpdServiceService {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+  }
 
   storeLoginData(loginData: any[]) {
     const header = new Headers({'Content-Type': 'application/json'});
@@ -22,12 +23,20 @@ export class PgpdServiceService {
     const header = new Headers({'Content-Type': 'application/json'});
     return this.http.post('http://192.168.1.8:3000/demi/register', formData, {headers: header})
   }
+
   storeAnnouncementData(formData: any[]) {
     const header = new Headers({'Content-Type': 'application/json'});
     return this.http.post('http://192.168.1.8:3000/anounce/make', formData, {headers: header})
   }
+
   storeLecturerData(formData: any[]) {
     const header = new Headers({'Content-Type': 'application/json'});
     return this.http.post('http://192.168.1.8:3000/dosente/register', formData, {headers: header})
   }
+
+  //**************************************getMethods**************************************************
+  getStudentInfo() {
+    return this.http.get('https://demi-371a7.firebaseio.com/data.json');
+  }
 }
+
