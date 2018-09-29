05428c669f3c71ef6dff736c2c762584b4200f13
EdwardAdriaan <42216791+EdwardAdriaan@users.noreply.github.com>
2018-09-29 13:15:58 +0200

    home-page/info-page
    general formatting.

--- DemiLogger/DemiLogger/src/app/login/login.component.ts
+++ DemiLogger/DemiLogger/src/app/login/login.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {PgpdServiceService} from '../pgpd-service.service';
 import { Router, RouterModule } from '@angular/router';
-import * as jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-login',
